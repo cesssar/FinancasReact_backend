3adf28818d48000819ac33b98451ecf38c9c0055
Cesar Steinmeier <cesssar@me.com>
2023-08-07 22:46:06 -0300

    api backend para testes

---
+++ api/main.py
@@ -0,0 +1,67 @@
+from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+import json
+
+from mock import *
+
+app = FastAPI()
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/saldos/contacorrente", tags=["Saldos"])
+def saldo_conta_corrente():
+    return {"saldo" : saldos["contacorrente"]}
+
+@app.get("/saldos/faturascredito", tags=["Saldos"])
+def faturas_credito():
+    return {"saldo" : saldos["faturascredito"]}
+
+@app.get("/saldos/limitecredito", tags=["Saldos"])
+def limite_credito():
+    return {"saldo" : saldos["limitecredito"]}
+
+@app.get("/saldos/limitealimentacao", tags=["Saldos"])
+def limite_alimentacao():
+    return {"saldo" : saldos["limitealimentacao"]}
+
+@app.delete("/contas/debitaconta/{valor}", tags=["Contas"])
+def debita_conta(valor: float):
+    if saldos["contacorrente"] > valor:
+        saldos["contacorrente"] -= valor
+        return {"saldo": saldos["contacorrente"]}
+    else:
+        return {"message": "saldo insuficiente"}
+
+@app.put("/contas/lancacredito/{valor}", tags=["Contas"])
+def lanca_credito(valor: float):
+    if saldos["limitecredito"] > valor:
+        saldos["faturascredito"] += valor
+        saldos["limitecredito"] -= valor
+        return {"saldo": saldos["faturascredito"]}
+    else:
+        return {"message": "limite de crédito insuficiente"}
+
+@app.delete("/contas/debitaalimentacao/{valor}", tags=["Contas"])
+def debita_conta(valor: float):
+    if saldos["limitealimentacao"] > valor:
+        saldos["limitealimentacao"] -= valor
+        return {"saldo": saldos["limitealimentacao"]}
+    else:
+        return {"message": "saldo insuficiente"}
+
+@app.put("/contas/pagafatura/{valor}", tags=["Contas"])
+def lanca_credito(valor: float):
+    saldos["limitecredito"] += valor
+    return {"saldo": saldos["limitecredito"]}
---
+++ api/mock.py
@@ -0,0 +1,6 @@
+saldos = {
+    "contacorrente"        : 200.45,
+    "faturascredito"       : 150.15,
+    "limitecredito"        : 500.00,
+    "limitealimentacao"    : 320.78
+}
