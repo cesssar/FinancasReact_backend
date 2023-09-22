49c4f85e057889f1fa724e46680655f21670284d
Cesar Steinmeier <cesssar@me.com>
2023-09-21 23:51:21 -0300

    retirado HTTPS e ajustado endpoints para proxy

--- api/main.py
+++ api/main.py
@@ -26,8 +26,6 @@ app = FastAPI(
         "name": "Apache 2.0",
         "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
     },
-    ssl_keyfile="./cert/steinmeier.key",
-    ssl_certfile="./cert/steinmeier.crt",
 )
 
 origins = [
@@ -48,7 +46,11 @@ def get_userId(db: Session, request: Request):
     id = UsuarioRepository.getId(db, usuario)
     return id
 
-@app.post("/login", tags=["Autenticação"], summary="Gera token para demais endpoints")
+@app.get("/backend/")
+async def root():
+    return {"message": "Backend para o front React"}
+
+@app.post("/backend/login", tags=["Autenticação"], summary="Gera token para demais endpoints")
 async def login_user(db: Session = Depends(get_db),user: UsuarioLogin = Body(...)):
     id = UsuarioRepository.login(db, user.login, user.senha)
     if id:
@@ -56,24 +58,24 @@ async def login_user(db: Session = Depends(get_db),user: UsuarioLogin = Body(...
     raise HTTPException(status_code=401, detail="Credenciais inválidas")
 
 
-@app.get("/saldo/contas", tags=["Conta"],  summary="Retorna saldo das contas", dependencies=[Depends(JWTBearer())])
+@app.get("/backend/saldo/contas", tags=["Conta"],  summary="Retorna saldo das contas", dependencies=[Depends(JWTBearer())])
 def saldo_contas(r: Request, db: Session = Depends(get_db)) -> float:
     id_usuario = get_userId(db, r)
     saldo = ContaRepository.saldo(db, id_usuario)
     return jsonable_encoder(saldo)
 
-@app.get("/cadastros/conta", response_model=list[ContaResponse], tags=["Conta"],  summary="Lista as contas cadastradas", dependencies=[Depends(JWTBearer())])
+@app.get("/backend/cadastros/conta", response_model=list[ContaResponse], tags=["Conta"],  summary="Lista as contas cadastradas", dependencies=[Depends(JWTBearer())])
 def listar_conta(r: Request,db: Session = Depends(get_db)):
     id_usuario = get_userId(db, r)
     contas = ContaRepository.listar(db, id_usuario)
     return [ContaResponse.from_orm(conta) for conta in contas]
 
-@app.get("/cadastros/conta/{id}", tags=["Conta"],  summary="Retorna detalhes de uma conta", dependencies=[Depends(JWTBearer())])
+@app.get("/backend/cadastros/conta/{id}", tags=["Conta"],  summary="Retorna detalhes de uma conta", dependencies=[Depends(JWTBearer())])
 def get_lancamento(id: int, db: Session = Depends(get_db)):
     c = ContaRepository.get_conta(db, id)
     return jsonable_encoder(c)
 
-@app.post("/cadastros/conta", response_model=ContaResponse, status_code=status.HTTP_201_CREATED, tags=["Conta"],  summary="Cadastra uma nova conta", dependencies=[Depends(JWTBearer())])
+@app.post("/backend/cadastros/conta", response_model=ContaResponse, status_code=status.HTTP_201_CREATED, tags=["Conta"],  summary="Cadastra uma nova conta", dependencies=[Depends(JWTBearer())])
 def criar_conta(r: Request, request: ContaRequest, db: Session = Depends(get_db)):
     c = Conta(**request.model_dump())
     id_usuario = get_userId(db, r)
@@ -81,7 +83,7 @@ def criar_conta(r: Request, request: ContaRequest, db: Session = Depends(get_db)
     conta = ContaRepository.salvar(db, c)
     return ContaResponse.from_orm(conta)
 
-@app.delete("/cadastros/conta/{id}", status_code=status.HTTP_204_NO_CONTENT, tags=["Conta"],  summary="Deleta uma conta desde que zerada e não esteja em uso", dependencies=[Depends(JWTBearer())])
+@app.delete("/backend/cadastros/conta/{id}", status_code=status.HTTP_204_NO_CONTENT, tags=["Conta"],  summary="Deleta uma conta desde que zerada e não esteja em uso", dependencies=[Depends(JWTBearer())])
 def deletar_conta(id: int, db: Session = Depends(get_db)):
     if not ContaRepository.localizar(db, id):
         raise HTTPException(
@@ -93,7 +95,7 @@ def deletar_conta(id: int, db: Session = Depends(get_db)):
         )
     return Response(status_code=status.HTTP_200_OK)
 
-@app.put("/cadastros/conta/{id}/{valor}", tags=["Conta"],  summary="Debita ou credita um valor a uma conta", dependencies=[Depends(JWTBearer())])
+@app.put("/backend/cadastros/conta/{id}/{valor}", tags=["Conta"],  summary="Debita ou credita um valor a uma conta", dependencies=[Depends(JWTBearer())])
 def debitar_creditar(id: int, valor: float, db: Session = Depends(get_db)):
     if not ContaRepository.localizar(db, id):
         raise HTTPException(
@@ -106,25 +108,25 @@ def debitar_creditar(id: int, valor: float, db: Session = Depends(get_db)):
     return Response(status_code=status.HTTP_200_OK)
 
 
-@app.get("/fatura/cartaocredito", tags=["Cartão Crédito"],  summary="Retorna valor da fatura atual", dependencies=[Depends(JWTBearer())])
+@app.get("/backend/fatura/cartaocredito", tags=["Cartão Crédito"],  summary="Retorna valor da fatura atual", dependencies=[Depends(JWTBearer())])
 def fatura_atual(r: Request,db: Session = Depends(get_db)) -> float:
     id_usuario = get_userId(db, r)
     fatura = CartaoCreditoRepository.fatura_atual(db, id_usuario)
     return jsonable_encoder(fatura) if fatura else None
 
-@app.get("/limite/cartaocredito", tags=["Cartão Crédito"],  summary="Retorna limite disponível", dependencies=[Depends(JWTBearer())])
+@app.get("/backend/limite/cartaocredito", tags=["Cartão Crédito"],  summary="Retorna limite disponível", dependencies=[Depends(JWTBearer())])
 def fatura_atual(r: Request,db: Session = Depends(get_db), tipo: str = 'c') -> float:
     id_usuario = get_userId(db, r)
     limite = CartaoCreditoRepository.limite_credito(db, id_usuario, tipo)
     return jsonable_encoder(limite) if limite else None
 
-@app.get("/cadastros/cartaocredito", response_model=list[CartaoCreditoResponse], tags=["Cartão Crédito"],  summary="Lista os cartões de crédito cadastrados", dependencies=[Depends(JWTBearer())])
+@app.get("/backend/cadastros/cartaocredito", response_model=list[CartaoCreditoResponse], tags=["Cartão Crédito"],  summary="Lista os cartões de crédito cadastrados", dependencies=[Depends(JWTBearer())])
 def listar_cartaocredito(r: Request,db: Session = Depends(get_db)):
     id_usuario = get_userId(db, r)
     contas = CartaoCreditoRepository.listar(db, id_usuario)
     return [CartaoCreditoResponse.from_orm(conta) for conta in contas]
 
-@app.post("/cadastros/cartaocredito", response_model=CartaoCreditoResponse, status_code=status.HTTP_201_CREATED, tags=["Cartão Crédito"],  summary="Cadastra um novo cartão de crédito", dependencies=[Depends(JWTBearer())])
+@app.post("/backend/cadastros/cartaocredito", response_model=CartaoCreditoResponse, status_code=status.HTTP_201_CREATED, tags=["Cartão Crédito"],  summary="Cadastra um novo cartão de crédito", dependencies=[Depends(JWTBearer())])
 def criar_cartao(r: Request, request: CartaoCreditoRequest, db: Session = Depends(get_db)):
     id_usuario = get_userId(db, r)
     cartao = CartaoCredito(**request.model_dump())
@@ -132,7 +134,7 @@ def criar_cartao(r: Request, request: CartaoCreditoRequest, db: Session = Depend
     c = CartaoCreditoRepository.salvar(db, cartao)
     return CartaoCreditoRepository.from_orm(c)
 
-@app.delete("/cadastros/cartaocredito/{id}", status_code=status.HTTP_204_NO_CONTENT, tags=["Cartão Crédito"],  summary="Deleta um cartão de crédito zerado e sem lançamentos", dependencies=[Depends(JWTBearer())])
+@app.delete("/backend/cadastros/cartaocredito/{id}", status_code=status.HTTP_204_NO_CONTENT, tags=["Cartão Crédito"],  summary="Deleta um cartão de crédito zerado e sem lançamentos", dependencies=[Depends(JWTBearer())])
 def deletar_cartao(id: int, db: Session = Depends(get_db)):
     if not CartaoCreditoRepository.localizar(db, id):
         raise HTTPException(
@@ -144,7 +146,7 @@ def deletar_cartao(id: int, db: Session = Depends(get_db)):
         )
     return Response(status_code=status.HTTP_200_OK)
 
-@app.put("/cadastros/cartaocredito/{id}/{valor}", tags=["Cartão Crédito"],  summary="Lança uma compra/despesa em um cartão de crédito", dependencies=[Depends(JWTBearer())])
+@app.put("/backend/cadastros/cartaocredito/{id}/{valor}", tags=["Cartão Crédito"],  summary="Lança uma compra/despesa em um cartão de crédito", dependencies=[Depends(JWTBearer())])
 def lancar(id: int, valor: float, db: Session = Depends(get_db)):
     if not CartaoCreditoRepository.localizar(db, id):
         raise HTTPException(
@@ -156,7 +158,7 @@ def lancar(id: int, valor: float, db: Session = Depends(get_db)):
         )
     return Response(status_code=status.HTTP_200_OK)
 
-@app.put("/cadastros/cartaocredito/pagarfatura/{id}/{valor}", tags=["Cartão Crédito"],  summary="Informa pagamento da fatura recompondo limite", dependencies=[Depends(JWTBearer())])
+@app.put("/backend/cadastros/cartaocredito/pagarfatura/{id}/{valor}", tags=["Cartão Crédito"],  summary="Informa pagamento da fatura recompondo limite", dependencies=[Depends(JWTBearer())])
 def pagar(id: int, valor: float, db: Session = Depends(get_db)):
     if not CartaoCreditoRepository.localizar(db, id):
         raise HTTPException(
@@ -170,13 +172,13 @@ def pagar(id: int, valor: float, db: Session = Depends(get_db)):
 
 
 
-@app.get("/cadastros/categoria", response_model=list[CategoriaResponse], tags=["Categoria"],  summary="Lista as categorias cadastradas", dependencies=[Depends(JWTBearer())])
+@app.get("/backend/cadastros/categoria", response_model=list[CategoriaResponse], tags=["Categoria"],  summary="Lista as categorias cadastradas", dependencies=[Depends(JWTBearer())])
 def listar_categoria(r: Request,db: Session = Depends(get_db)):
     id_usuario = get_userId(db, r)
     categorias = CategoriaRepository.listar(db, id_usuario)
     return [CategoriaResponse.from_orm(categoria) for categoria in categorias]
 
-@app.post("/cadastros/categoria", response_model=CategoriaResponse, status_code=status.HTTP_201_CREATED, tags=["Categoria"],  summary="Cadastra uma nova categoria", dependencies=[Depends(JWTBearer())])
+@app.post("/backend/cadastros/categoria", response_model=CategoriaResponse, status_code=status.HTTP_201_CREATED, tags=["Categoria"],  summary="Cadastra uma nova categoria", dependencies=[Depends(JWTBearer())])
 def criar_categoria(r: Request, request: CategoriaRequest, db: Session = Depends(get_db)):
     id_usuario = get_userId(db, r)
     categoria = Categoria(**request.model_dump())
@@ -184,7 +186,7 @@ def criar_categoria(r: Request, request: CategoriaRequest, db: Session = Depends
     c = CategoriaRepository.salvar(db, categoria)
     return CategoriaResponse.from_orm(c)
 
-@app.delete("/cadastros/categoria/{id}", status_code=status.HTTP_204_NO_CONTENT, tags=["Categoria"],  summary="Deleta uma categoria sem uso", dependencies=[Depends(JWTBearer())])
+@app.delete("/backend/cadastros/categoria/{id}", status_code=status.HTTP_204_NO_CONTENT, tags=["Categoria"],  summary="Deleta uma categoria sem uso", dependencies=[Depends(JWTBearer())])
 def deletar_categoria(id: int, db: Session = Depends(get_db)):
     if not CategoriaRepository.localizar(db, id):
         raise HTTPException(
@@ -198,18 +200,18 @@ def deletar_categoria(id: int, db: Session = Depends(get_db)):
 
 
 
-@app.get("/cadastros/lancamento/{mes}/{ano}", tags=["Lançamento"],  summary="Lista os lançamentos cadastrados", dependencies=[Depends(JWTBearer())])
+@app.get("/backend/cadastros/lancamento/{mes}/{ano}", tags=["Lançamento"],  summary="Lista os lançamentos cadastrados", dependencies=[Depends(JWTBearer())])
 def listar_lancamento(r: Request, mes: int, ano: int, db: Session = Depends(get_db)):
     id_usuario = get_userId(db, r)
     l = LancamentoRepository.listar(db, mes, ano, id_usuario)
     return jsonable_encoder(l)
 
-@app.get("/cadastros/lancamento/{id}", tags=["Lançamento"],  summary="Retorna detalhes de um lançamento", dependencies=[Depends(JWTBearer())])
+@app.get("/backend/cadastros/lancamento/{id}", tags=["Lançamento"],  summary="Retorna detalhes de um lançamento", dependencies=[Depends(JWTBearer())])
 def get_lancamento(id: int, db: Session = Depends(get_db)):
     l = LancamentoRepository.get_lancamento(db, id)
     return jsonable_encoder(l)
 
-@app.post("/cadastros/lancamento", status_code=status.HTTP_201_CREATED, tags=["Lançamento"],  summary="Cadatra um novo lançamento", dependencies=[Depends(JWTBearer())])
+@app.post("/backend/cadastros/lancamento", status_code=status.HTTP_201_CREATED, tags=["Lançamento"],  summary="Cadatra um novo lançamento", dependencies=[Depends(JWTBearer())])
 def criar_lancamento(r: Request, request: LancamentoRequest, db: Session = Depends(get_db)):
     id_usuario = get_userId(db, r)
     lancamento = Lancamento(**request.model_dump())
@@ -221,7 +223,7 @@ def criar_lancamento(r: Request, request: LancamentoRequest, db: Session = Depen
         )
     return Response(status_code=status.HTTP_201_CREATED)
 
-@app.delete("/cadastros/lancamento/{id}", status_code=status.HTTP_204_NO_CONTENT, tags=["Lançamento"],  summary="Deleta um lançamento", dependencies=[Depends(JWTBearer())])
+@app.delete("/backend/cadastros/lancamento/{id}", status_code=status.HTTP_204_NO_CONTENT, tags=["Lançamento"],  summary="Deleta um lançamento", dependencies=[Depends(JWTBearer())])
 def deletar_lancamento(id: int, db: Session = Depends(get_db)):
     if not LancamentoRepository.localizar(db, id):
         raise HTTPException(
@@ -234,7 +236,7 @@ def deletar_lancamento(id: int, db: Session = Depends(get_db)):
     return Response(status_code=status.HTTP_200_OK)
 
 
-@app.post("/cadastros/qrcode/" , tags=["Lançamento"],  summary="", dependencies=[Depends(JWTBearer())])
+@app.post("/backend/cadastros/qrcode/" , tags=["Lançamento"],  summary="", dependencies=[Depends(JWTBearer())])
 def qrcode(r: Request,request: QrcodeRequest, db: Session = Depends(get_db)):
     id_usuario = get_userId(db, r)
     dados = ExtrairDados(str(request.link)).extrair()
